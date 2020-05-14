03105b2747ed9ec57596265e9f42731e9e118dc8
David Kalcic <kalcicd@oregonstate.edu>
2020-05-14 16:33:55 -0700

    refactor to use json logfile instead of txt logfile

--- status-page.py
+++ status-page.py
@@ -6,8 +6,9 @@ import requests
 class StatusPage:
     def __init__(self):
         config_data_file = open(sys.argv[1])
-        self.log_file = open(sys.argv[2])
+        log_file = open(sys.argv[2])
         self.config_json = json.load(config_data_file)
+        self.log_json = json.load(log_file)
         self.access_token = self.config_json['access_token']
         self.base_url = self.config_json['base_url']
         self.header = {'x-cachet-token': self.access_token}
@@ -23,20 +24,6 @@ class StatusPage:
             'watching': [],
             'fixed': []
         }
-        self.passed_tests = []
-        self.failed_tests = []
-
-    def parseLogfile(self):
-        # success = 'Passing cases:'
-        failure = 'The following API(s) returned errors:'
-        end = "Build step 'Execute shell' marked build as"
-        file_contents = self.log_file.read()
-
-        if failure in file_contents:
-            failing_index = file_contents.index(failure) + len(failure)
-            end_index = file_contents.index(end)
-            errors = file_contents[failing_index:end_index]
-            print(errors)
 
     def getAllStatuses(self):
         response = requests.get(f'{self.base_url}/components', self.header)
@@ -67,11 +54,10 @@ class StatusPage:
     # Should we update incident status to resolved if test succeeds on API
     # with open issue?
     def updateStatuses(self):
-        print("Updating Statuses")
+        print('Updating statuses')
 
 
 if __name__ == '__main__':
     status_page = StatusPage()
-    status_page.parseLogfile()
     status_page.getAllStatuses()
     status_page.getAllIncidents()
