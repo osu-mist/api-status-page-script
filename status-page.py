9276de0dda1e7937e68f10ccc88d3625f7bd3b0d
David Kalcic <kalcicd@oregonstate.edu>
2020-05-18 14:00:00 -0700

    create incidents when a test fails that doesn't already have an open incident.

--- status-page.py
+++ status-page.py
@@ -1,6 +1,7 @@
 import json
 import sys
 import requests
+from urllib.parse import urlparse
 
 
 class StatusPage:
@@ -51,13 +52,60 @@ class StatusPage:
             if status == 4:
                 self.incidents['fixed'].append(incident)
 
-    # Should we update incident status to resolved if test succeeds on API
-    # with open issue?
     def updateStatuses(self):
-        print('Updating statuses')
+        component_map = {
+            'v1/academic-disciplines': 3,
+            'v1/advisors': 4,
+            'v1/xeapps': 5,
+            'v1/beaverbus': 6,
+            'v1/directory': 7,
+            'v2/directory': 8,
+            'v1/finance': 9,
+            'v1/hr': 10,
+            'v1/identify': 11,
+            'v1/locations': 12,
+            'v1/oauth2': 13,
+            'v1/onbase': 14,
+            'v1/persons': 15,
+            'v1/staff-fee-privilege': 16,
+            'v1/students': 17,
+            'v1/terms': 18,
+            'v1/textbooks': 19
+        }
+
+        # first, find all open incidents.
+        open_incidents = []
+        for incident_status in self.incidents:
+            if incident_status != 'fixed':
+                for incident in self.incidents[incident_status]:
+                    open_incidents.append(incident)
+
+        # then, if there are any test failures, open an incident if there is none open already
+        for failed_test in self.log_json['failed_tests']:
+            already_reported = False
+            parsed_url = urlparse(failed_test['base_url'])
+            split_path = parsed_url.path.split('/')
+            api_version = split_path[1]
+            api_name = split_path[2]
+            version_and_name = f'{api_version}/{api_name}'
+            print(version_and_name)
+            for incident in open_incidents:
+                if component_map[version_and_name] == incident['component_id']:
+                    already_reported = True
+            if not already_reported:
+                body = {
+                    'name': f'Jenkins-reported downtime for {api_name} {api_version}',
+                    'message': f'Jenkins has reported an issue with {api_name} api. This issue is under investigation.',
+                    'status': 1,
+                    'visible': 1,
+                    'component_id': component_map[version_and_name],
+                    'component_status': 2,
+                }
+                response = requests.post(f'{self.base_url}/incidents', headers=self.header, data=body)
 
 
 if __name__ == '__main__':
     status_page = StatusPage()
     status_page.getAllStatuses()
     status_page.getAllIncidents()
+    status_page.updateStatuses()
