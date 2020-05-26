89c9000f081dcdbb89a9f3e3a788ce389c087a81
David Kalcic <kalcicd@oregonstate.edu>
2020-05-26 13:45:08 -0700

    fix update_component_status() logic and fix linting errors

--- status-page.py
+++ status-page.py
@@ -23,45 +23,45 @@ class StatusPage:
             help='Path to api_status.json',
             required=True
         )
-        args, unknown_args = parser.parse_known_args()
+        args = parser.parse_known_args()[0]
         self.config_json = json.load(open(args.config_path))
         self.api_status_json = json.load(open(args.api_status_path))
         self.access_token = self.config_json['access_token']
         self.base_url = self.config_json['base_url']
         self.api_map = self.config_json['api_map']
         self.header = {'x-cachet-token': self.access_token}
-        self.broken_components = []
+        self.all_components = []
         self.open_incidents = []
 
-    def get_broken_components(self):
-        """Fetches all apis not marked as 'Operational'"""
+    def get_components(self):
+        """Fetch all apis"""
         try:
             response = requests.get(f'{self.base_url}/components', self.header)
         except RequestException as err:
             print(f'REQUEST ERROR! Unable to get components.\n{err}')
             sys.exit(1)
         for component in response.json()['data']:
-            if component['status'] != 1:
-                self.broken_components.append(component)
+            self.all_components.append(component)
 
     def get_all_open_incidents(self):
         """Fetches all incidents not marked as 'Resolved'"""
+        self.open_incidents = []
         try:
             response = requests.get(f'{self.base_url}/incidents', self.header)
         except RequestException as err:
             print(f'REQUEST ERROR! Unable to get incidents.\n{err}')
             sys.exit(1)
         for incident in response.json()['data']:
-            status = incident['status']
-            if status != 4:
+            if incident['status'] != 4:
                 self.open_incidents.append(incident)
 
     def update_incident_status(self):
-        """Goes through all passed and failed tests, resolves open incidents with a passed test, and creates new
-        incidents for failed tests."""
+        """Goes through all passed and failed tests, resolves open incidents
+        with a passed test, and creates new incidents for failed tests."""
         api_map = self.api_map
 
-        # If there are any test failures, open an incident if there is none open already
+        # If there are any test failures, open an incident if there is none
+        # open already
         for failed_test in self.api_status_json['failed_tests']:
             already_reported = False
             parsed_url = urlparse(failed_test['base_url'])
@@ -73,7 +73,7 @@ class StatusPage:
             if version_and_name in api_map:
                 component_id = api_map[version_and_name]
                 for incident in self.open_incidents:
-                    if component_id == incident['component_id']:
+                    if failed_test['base_url'] == incident['name']:
                         already_reported = True
                 if not already_reported:
                     body = {
@@ -87,13 +87,16 @@ class StatusPage:
 
                     }
                     try:
-                        response = requests.post(f'{self.base_url}/incidents', headers=self.header, data=body)
+                        response = requests.post(f'{self.base_url}/incidents',
+                                                 headers=self.header,
+                                                 data=body)
                     except RequestException as err:
                         print(f'REQUEST ERROR! Incident not posted:\n{err}')
                         sys.exit(1)
                     print(f'Posted New Incident: {response}')
 
-        # then parse through passed tests and resolve any previously opened incidents if it shares the same endpoint
+        # then parse through passed tests and resolve any previously opened
+        # incidents if it shares the same endpoint
         for passed_test in self.api_status_json['passed_tests']:
             already_reported = False
             parsed_url = urlparse(passed_test['base_url'])
@@ -106,8 +109,7 @@ class StatusPage:
             if version_and_name in api_map:
                 component_id = api_map[version_and_name]
                 for incident in self.open_incidents:
-                    if component_id == incident['component_id'] \
-                            and passed_test['base_url'] == incident['name']:
+                    if passed_test['base_url'] == incident['name']:
                         already_reported = True
                         incident_id = incident['id']
 
@@ -123,23 +125,27 @@ class StatusPage:
                             headers=self.header,
                             data=body
                         )
+                        print(f'Updated Incident Status: {response}')
                     except RequestException as err:
-                        print(f'REQUEST ERROR! Unable to update incident.\n{err}')
-                        sys.exit(1)
-                    print(f'Updated Incident Status: {response}')
+                        sys.exit(
+                            f'REQUEST ERROR! Unable to update incident.\n{err}'
+                        )
 
     def update_component_status(self):
-        """Mark all components with open incidents with 'Performance Issues' status"""
-        for broken_api in self.broken_components:
-            for open_incident in self.open_incidents:
-                if open_incident['component_id'] == broken_api['id']:
-                    body = {'status': 2}
-                    try:
-                        requests.put(f'{self.base_url}/components/{broken_api["id"]}', headers=self.header, data=body)
-                    except RequestException as err:
-                        print(f'REQUEST ERROR! Unable to update component status.\n{err}')
-                        sys.exit(1)
-                    break
+        """Mark all components with open incidents with 'Performance Issues'
+        status"""
+        for open_incident in self.open_incidents:
+            body = {'status': 2}
+            try:
+                requests.put(
+                    f'{self.base_url}/components/{open_incident["component_id"]}',
+                    headers=self.header, data=body)
+                print((f'Marking component {open_incident["component_id"]} as '
+                       'having "Performance Issues"'))
+            except RequestException as err:
+                sys.exit(
+                    f'REQUEST ERROR! Unable to update component status.\n{err}'
+                )
 
 
 if __name__ == '__main__':
@@ -149,9 +155,6 @@ if __name__ == '__main__':
     status_page.get_all_open_incidents()
     status_page.update_incident_status()
 
-    # get open incidents again after they are updated
+    # get open incidents again after they were updated, then update api status
     status_page.get_all_open_incidents()
-
-    # get all apis with reported issues and reassign status based on if there's any open incidents
-    status_page.get_broken_components()
     status_page.update_component_status()
